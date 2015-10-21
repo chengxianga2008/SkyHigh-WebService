9a1eddde7ebeb33cd7f101fa49d717c85f2810ed
Jack Carrick Xiang Cheng <chengxianga2008@yahoo.com>
2015-10-21 16:53:26 +1100

    .gitignore is now working

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,17 +11,32 @@
 |
 */
 
+use mikehaertl\pdftk\FdfFile;
+use mikehaertl\pdftk\Pdf;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get ( '/ws/genesystel_pdf', ['as' => 'genesystel_pdf', function () {
+Route::get( '/ws/genesystel_pdf', ['as' => 'genesystel_pdf', function () {
 	
-// 		$name = Request::input('name');
-// 		$email = Request::input('email');
-// 		$send_message = Request::input('message');
+	$contract_number = Request::input('contract_number');
+	$code = Request::input('code');
 	
-	    error_log("enter");
-			return "success";
+	if($code != 'GoqKRmEmABshvIuRxhL3duxhdMvclbyX'){
+		return;
+	}
+	
+ 		
+//	asset('css/bootstrap.min.css');
+	$pdf = new Pdf('resources/Genesystel_Landline_combined_contract.pdf');
+	$pdf->fillForm(array('Contract ID'=> $contract_number))
+	    ->needAppearances()
+	    ->saveAs('filled/Genesystel_Landline_combined_contract.pdf');
+	
+	return response(file_get_contents('filled/Genesystel_Landline_combined_contract.pdf'), 200)
+        ->header('Content-type', 'application/pdf')
+	    ->header('Content-Disposition', 'inline; filename="Genesystel_Landline_combined_contract.pdf"');
+			
 }]);
